28e4960f03d8a3533991496c6d7ac23679a02263
1618097 <1618097@cs16097>
2018-11-23 00:44:43 +0900

    commit

--- src/speechtotext2/SpeechtoText2_lib.java
+++ src/speechtotext2/SpeechtoText2_lib.java
@@ -1,5 +1,60 @@
 package speechtotext2;
 
-public class SpeechtoText2_lib {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
+
+public class SpeechtoText2_lib {
+	
+	SpeechToText service;
+	RecognizeOptions options = null;
+	
+	public SpeechtoText2_lib(File audio) {
+	    service = new SpeechToText();
+	    service.setUsernameAndPassword("b755a0d3-8974-4499-b202-cde225260323", "NwBqr4j0wFUC");
+	    
+	    try {
+			options = new RecognizeOptions.Builder()
+				.model("ja-JP_BroadbandModel")
+			    .audio(audio)
+			    .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
+			    .build();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+
+	}
+
+	public SpeechRecognitionResults getTranscript() {
+		SpeechRecognitionResults transcript = service.recognize(options).execute();
+		return transcript;
+	}
+	public void getJson(SpeechRecognitionResults transcript)
+	{
+		String s = String.valueOf(transcript);
+		ObjectMapper mapper = new ObjectMapper();
+		MySQL mysql = new MySQL();
+		
+		try {
+			JsonNode node = mapper.readTree(s);
+			for (int i = 0; i < node.get("results").size(); i++) {
+			String text = node.get("results").get(i).get("alternatives").get(0).get("transcript").toString();
+			double confidence = node.get("results").get(i).get("alternatives").get(0).get("confidence").asDouble();
+			System.out.println("transcript" + text);
+			System.out.println("confidence" + confidence);
+			mysql.updateImage(text, confidence);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/speechtotext2/SpeechtoText2_main.java
+++ src/speechtotext2/SpeechtoText2_main.java
@@ -1,5 +1,28 @@
 package speechtotext2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
+
 public class SpeechtoText2_main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		    SpeechtoText2_lib slib = new SpeechtoText2_lib(new File("audio/2.wav"));
+    		SpeechRecognitionResults transcript = slib.getTranscript();
+
+		    System.out.println(transcript);
+
+		    slib.getJson(transcript);
+
+	}
 
 }
